730fda16ecb4e314764d064bfdcc255557fcd64a
Gufron <gupron.nurjalil14@gmail.com>
2025-08-26 10:26:50 +0700

    wilayah indonesia

---
+++ app/Models/District.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class District extends Model
+{
+    //
+    public function regency()
+    {
+        return $this->belongsTo(Regency::class);
+    }
+
+    public function villages()
+    {
+        return $this->hasMany(Village::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::unprepared(file_get_contents((database_path('seeders/indonesia.sql'))));
+
         User::updateOrCreate(
             ['email' => 'admin@attaufiq.com'], // kunci unik
             [

57cc7f934ee49dd544377afafedca6a33fa53409
Gufron <gupron.nurjalil14@gmail.com>
2025-09-02 18:55:46 +0700

    add media route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use App\Livewire\Home;
 use App\Livewire\Kesiswaan;
-use App\Livewire\PendaftaranSiswa;
 use App\Livewire\ProfilMadrasah;
+use App\Livewire\PendaftaranSiswa;
 use Illuminate\Support\Facades\Route;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 Route::get('/', Home::class)->name('/');
 
 Route::get('ppdb', PendaftaranSiswa::class)->name('ppdb');
 Route::get('profil-madrasah', ProfilMadrasah::class)->name('profil-madrasah');
 Route::get('kesiswaan', Kesiswaan::class)->name('kesiswaan');
+
+Route::get('/media/{media}', function (Media $media) {
+    return response()->file($media->getPath());
+})->name('media.show');
